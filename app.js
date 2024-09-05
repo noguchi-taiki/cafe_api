8ea65e85b428c1459860f1730f1029778a2ea88d
noguchi-taiki <taiki.13n1015@gmail.com>
2024-09-05 22:31:58 +0900

    DBの値の参照に成功
    今後：reqにて現在地を取得してそこから近いものを10件or20件表示するというものにする。
    またフロント側でapiにて取得してきたデータの表示させる

--- app.js
+++ app.js
@@ -2,29 +2,29 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const app = express();
 const port = 8000;
+let connection;
 
-const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    // prot: "3306",非推奨らしいので書かない
-    database: "cafe_db",
-    user: "root",
-    password: "root",
-})
+(async()=>{
+    connection = await mysql.createConnection({
+        host: "127.0.0.1",
+        // prot: "3306",非推奨らしいので書かない
+        database: "cafe_db",
+        user: "root",
+        password: "root",
+    })
+})()
 
-app.get('/',(req,res)=>{
-    res.send("テスト用");
-})
+// app.get('/',(req,res)=>{
+//     res.send("テスト用");
+// })
 
 app.get('/api',async(req,res)=>{
-    try {
-            const [results] = await connection.query('select * from tokyo limit 10;');
-            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-            res.status(201).json({msg: results});
-    } catch (error) {
-            console.log('mysqlの接続エラー'); 
-            res.status(500).json({msg: error})
-    }
-    
+    const responce = await connection.query('SELECT * FROM tokyo limit 5')
+    .then((responce)=>{
+        responce = responce[0];//responceの2番目(1)のオブジェクトにはfield？(DBの型)が挿入されている
+        console.log(responce);
+        res.json(responce);
+    })
 })
 
 app.listen(port, ()=>{
